4663a97dcdb945723ebdd9afdc714bee78e4aeb8
Ivan krashevskyi <xxxserafimxxx@ukr.net>
2023-12-09 12:37:18 +0200

    Updates

---
+++ static/js/NotFound.399cf39c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_pizza=self.webpackChunkreact_pizza||[]).push([[187],{4949:function(e,n,t){t.r(n);var u=t(184);n.default=function(){return(0,u.jsx)("h1",{children:"Not found"})}}}]);
+//# sourceMappingURL=NotFound.399cf39c.chunk.js.map
